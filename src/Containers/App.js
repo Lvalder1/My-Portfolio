f323ee23e648292d1ad9fe8f7f24d6e65a8a5a3b
Louis valder <louis_valder@hotmail.co.uk>
2020-10-06 21:20:21 +0100

    Added navigation component. More to be added

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import styles from "./App.module.scss";
+import Navigation from "../Components/Navigation";
 
 export default class App extends Component {
   render () {
     return (
-      <div className={styles.app}>Hello</div>
+      <div className={styles.app}><p>Hello</p>
+      <Navigation />
+      </div>
     );
   }
 }
