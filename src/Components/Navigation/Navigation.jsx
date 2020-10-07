c0bf54b9108bdeb7780c88f13b0df3f891ea131e
Louis valder <louis_valder@hotmail.co.uk>
2020-10-07 18:55:12 +0100

    Added links to navigation

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import styles from "./Navigation.module.scss";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default class Navigation extends Component {
     render () {
       return (
-        <div className={styles.app}>Hello</div>
+        <Navbar bg="light" expand="sm" fixed="top">
+        <Navbar.Brand className={styles.brand}>
+            <p id={styles.brandTop}>Louis Valder</p>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto mr-0">
+            <Nav.Link href="home">Home</Nav.Link>
+            <Nav.Link href="about">About</Nav.Link>
+            <Nav.Link href="skills">Skills</Nav.Link>
+            <Nav.Link href="contact">Contact</Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
       );
     }
   }
