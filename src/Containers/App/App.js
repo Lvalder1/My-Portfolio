79b6de9605a6d2c9bae6e71681b07b15a8235eae
Louis valder <louis_valder@hotmail.co.uk>
2020-10-08 12:45:34 +0100

    Added skills page

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styles from "./App.module.scss";
 import Navigation from "../../Components/Navigation";
 import About from "../About";
+import Skills from "../Skills";
 
 export default class App extends Component {
   render () {
@@ -9,6 +10,7 @@ export default class App extends Component {
       <div className={styles.app}>
       <Navigation />
       <About />
+      <Skills />
       </div>
     );
   }
---
+++ src/Containers/Skills/Skills.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import styles from "./Skills.module.scss";
+
+export default class Skills extends Component {
+    render () {
+        return (
+            <div className={styles.skills}>Skills</div>
+        );
+    }
+}
