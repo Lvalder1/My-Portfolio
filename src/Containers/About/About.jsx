6472c1436c7f4220e0da86906f1bd23fe9b1d1b9
Louis valder <louis_valder@hotmail.co.uk>
2020-10-18 17:30:57 +0100

    Changed mobile layout

--- src/Containers/About/About.jsx
+++ src/Containers/About/About.jsx
@@ -13,7 +13,7 @@ export default class About extends Component {
                     </div>
                     <div className={styles.aboutMeRight}>
                         <p>I recently completed a 12 week, intensive course at _nology in front end web development, 
-                        building interactive and responsive websites. I am hardworking and friendly, whilst maintaining a professional 
+                        building interactive and responsive websites as well as web based applications. I am hardworking and friendly, whilst maintaining a professional 
                         attitude at all times. I am also highly practical and enjoy technical problem solving. For my next role I wish 
                         to better utilise my technical skills and problem solving within the coding industry.</p>
                     </div>
