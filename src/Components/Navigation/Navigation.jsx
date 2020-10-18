2a2b6ed4662bb99bfe6a1858bbb52a41f556ec9c
Louis valder <louis_valder@hotmail.co.uk>
2020-10-18 17:11:47 +0100

    Added about me section

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 export default class Navigation extends Component {
     render () {
       return (
-        <Navbar bg="light" expand="sm" fixed="top">
+        <Navbar bg="light" expand="sm" static="top">
         <Navbar.Brand className={styles.brand}>
             <p id={styles.brandTop}>Louis Valder</p>
         </Navbar.Brand>
--- src/Containers/About/About.jsx
+++ src/Containers/About/About.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
 import styles from "./About.module.scss";
+import profile from "../../Images/Louis.png";
 
 export default class About extends Component {
     render () {
         return (
-            <div className={styles.about}>About</div>
+            <div className={styles.about}>
+                <h1>Hi I am <span>Louis Valder</span> I am a web developer!</h1>
+                <div id={styles.aboutMe}>
+                    <div className={styles.aboutMeLeft}>
+                        <img src={profile} alt="Profile"></img>
+                    </div>
+                    <div className={styles.aboutMeRight}>
+                        <p>I recently completed a 12 week, intensive course at _nology in front end web development, 
+                        building interactive and responsive websites. I am hardworking and friendly, whilst maintaining a professional 
+                        attitude at all times. I am also highly practical and enjoy technical problem solving. For my next role I wish 
+                        to better utilise my technical skills and problem solving within the coding industry.</p>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
