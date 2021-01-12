46f993b80fbebcb28f25257bb86c08a44273d7c5
Louis Valder <louis.valder@nology.io>
2021-01-12 18:02:38 +0000

    Added images to projects

--- src/Containers/Projects/Projects.jsx
+++ src/Containers/Projects/Projects.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import styles from "./Projects.module.scss";
+import rps from "../../Images/rps.png";
 
 export default class Projects extends Component {
     render () {
@@ -7,13 +8,14 @@ export default class Projects extends Component {
             <div className={styles.projects} id="projects">
                 <h3>Projects</h3>
                 <div className={styles.projectArea}>
-                   <a href="https://lvalder1.github.io/Rock-Paper-Scissors/" target="_blank" rel="noopener noreferrer">
-                        <div className={styles.project}>
+                    <div className={styles.project}>
+                        <a href="https://lvalder1.github.io/Rock-Paper-Scissors/" target="_blank" rel="noopener noreferrer">
+                            <img src={rps} alt="Rock-Paper-Scissors" className={styles.projectImage}></img>
                             <div className={styles.projectName}>
                             Rock, Paper, Scissors!
                             </div>
-                        </div>
-                    </a>
+                        </a>
+                    </div>
                     <div className={styles.project}></div>
                     <div className={styles.project}></div>
                 </div>
