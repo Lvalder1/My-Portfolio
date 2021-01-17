dc95fd7315c31716f45aa139a9e4fa200b0092b5
Louis Valder <louis.valder@nology.io>
2021-01-17 21:38:51 +0000

    Completed navigation routes

--- src/Containers/LandingPage/LandingPage.jsx
+++ src/Containers/LandingPage/LandingPage.jsx
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
-import About from "../About";
-import Project from "../Projects/Project"; 
+import Project from "../../Components/Project"; 
 import RPS from "../../Images/rps.png";
 
 export default class LandingPage extends Component {
     render() {
         return (     
                     <>
-                    <About />
+        
                     <Project
                         heading="Rock, Paper, Scissors"
                         link="https://lvalder1.github.io/Rock-Paper-Scissors/"
