a6272eebe299d7086d785ddc9064623f90d77812
Louis Valder <louis.valder@nology.io>
2021-01-24 17:04:09 +0000

    Added fontawsome to contact page

--- src/Containers/Contact/Contact.jsx
+++ src/Containers/Contact/Contact.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import styles from "./Contact.module.scss";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 export default class Contact extends Component {
     render () {
@@ -9,7 +10,7 @@ export default class Contact extends Component {
                 <h1>Contact</h1>
                 <div className={styles.iconContactBox}>
                     <div className={styles.iconBox}>
-                        {/* <FontAwesomeIcon /> */}
+                        <FontAwesomeIcon icon={faCoffee} />
                     </div>
                     <div className={styles.textBox}>
                         <a href="mailto:louis_valder@hotmail.co.uk"><h2>louis_valder@hotmail.co.uk</h2></a>
