bb948d2005821e16cabd1db094b1dc45acf50d14
Louis Valder <louis.valder@nology.io>
2021-01-24 16:32:07 +0000

    Added email to contact page

--- src/Containers/Contact/Contact.jsx
+++ src/Containers/Contact/Contact.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
 import styles from "./Contact.module.scss";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default class Contact extends Component {
     render () {
         return (
-            <div className={styles.contact} id="contact">Contact</div>
+            <div className={styles.contact} id="contact">
+                <h1>Contact</h1>
+                <div className={styles.iconContactBox}>
+                    <div className={styles.iconBox}>
+                        {/* <FontAwesomeIcon /> */}
+                    </div>
+                    <div className={styles.textBox}>
+                        <a href="mailto:louis_valder@hotmail.co.uk"><h2>louis_valder@hotmail.co.uk</h2></a>
+                    </div>
+                </div>
+            </div>
         );
     }
 } 
