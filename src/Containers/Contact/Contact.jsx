30c66a65d69ef4149fe92ae505c55425382b6eb6
Louis Valder <louis.valder@nology.io>
2021-01-28 21:57:27 +0000

    Completed desktop view for contact page. Mobile to follow.

--- src/Containers/Contact/Contact.jsx
+++ src/Containers/Contact/Contact.jsx
@@ -12,18 +12,23 @@ export default class Contact extends Component {
                 <div className={styles.iconContactBox}>
                     <h1>Contact</h1>
                         <p>Please feel free to contact me using the links bellow:</p>
-                    <div className={styles.textBox}>
-                        <a href="mailto:louis_valder@hotmail.co.uk"><FontAwesomeIcon icon={faEnvelope} className={styles.icon} size="lg" /></a>
-                        <a href="mailto:louis_valder@hotmail.co.uk">louis_valder@hotmail.co.uk</a>
-                    </div>
-                    <div className={styles.textBox}>
-                        <a href="https://www.linkedin.com/in/louisvalder/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} className={styles.icon} size="lg" /></a>
-                    </div>
-                    <div className={styles.textBox}>
-                        <a href="https://github.com/Lvalder1" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithubSquare} className={styles.icon} size="lg" /></a>
-                    </div>
-                    <div className={styles.textBox}>
-                        <a href={CV} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFileWord} className={styles.icon} size="lg" /></a>
+                    <div className={styles.contactLinks}>    
+                        <div className={styles.textBox}>
+                            <a href="mailto:louis_valder@hotmail.co.uk"><FontAwesomeIcon icon={faEnvelope} className={styles.icon} size="lg" /></a>
+                            <a href="mailto:louis_valder@hotmail.co.uk">louis_valder@hotmail.co.uk</a>
+                        </div>
+                        <div className={styles.textBox}>
+                            <a href="https://www.linkedin.com/in/louisvalder/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} className={styles.icon} size="lg" /></a>
+                            <a href="linkedin.com/in/louisvalder/" target="_blank" rel="noopener noreferrer">linkedin.com/in/louisvalder/</a>
+                        </div>
+                        <div className={styles.textBox}>
+                            <a href="https://github.com/Lvalder1" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithubSquare} className={styles.icon} size="lg" /></a>
+                            <a href="github.com/Lvalder1" target="_blank" rel="noopener noreferrer">github.com/Lvalder1</a>
+                        </div>
+                        <div className={styles.textBox}>
+                            <a href={CV} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFileWord} className={styles.icon} size="lg" /></a>
+                            <a href={CV} target="_blank" rel="noopener noreferrer">CV</a>
+                        </div>
                     </div>
                 </div>
             </div>
