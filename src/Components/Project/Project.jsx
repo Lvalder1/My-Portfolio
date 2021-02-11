66686b015fc46deff9b6678a1f910a1f0398f688
Louis Valder <louis.valder@nology.io>
2021-02-11 14:51:56 +0000

    testing mobile view of projects

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import styles from "./Project.module.scss";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
+import { faCode, faWindowMaximize } from '@fortawesome/free-solid-svg-icons'
 
 export default class Project extends Component {
     render () {
@@ -11,7 +11,14 @@ export default class Project extends Component {
                     <h3>{this.props.heading}</h3>
                     <img src={this.props.imgSrc} alt={this.props.imgAlt} className={styles.projectImage}></img>
                     <div className={styles.icons}>
-                        <a href={this.props.code} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithubSquare} className={styles.icon} size="lg" /></a>
+                        <div className={styles.iconTxt}>
+                            <a href={this.props.code} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faCode} className={styles.icon} />
+                            <p>Check Out the Code</p></a>
+                        </div>
+                        <div className={styles.iconTxt}>
+                            <a href={this.props.link} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faWindowMaximize} className={styles.icon} />
+                            <p>View the Project</p></a>
+                        </div>
                     </div>
                 </div>
             </div>
