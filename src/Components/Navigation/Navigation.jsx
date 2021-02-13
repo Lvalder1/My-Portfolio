b7d0c2de433cf0a232953cfe6836506db5a26849
Louis Valder <louis.valder@nology.io>
2021-02-13 15:08:14 +0000

    Added colour to active narbar page

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -4,6 +4,20 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import 'bootstrap/dist/css/bootstrap.css';
 
+
+const NavItem = props => {
+  const pageURI = window.location.pathname+window.location.search
+  const liClassName = (props.path === pageURI) ? "nav-item active" : "nav-item";
+  return (
+    <li className={styles[liClassName]}>
+      <a href={props.path}>
+        {props.name}
+        {(props.path === pageURI)}
+      </a>
+    </li>
+  );
+}
+
 export default class Navigation extends Component {
     render () {
       return (
@@ -11,13 +25,13 @@ export default class Navigation extends Component {
         <Navbar.Brand className={styles.brand}>
             <p id={styles.brandTop}>Louis Valder</p>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto ml-20">
-            <Nav.Link href="/" className={styles.navLink}>About</Nav.Link>
-            <Nav.Link href="skills" className={styles.navLink}>Skills</Nav.Link>
-            <Nav.Link href="projects" className={styles.navLink}>Projects</Nav.Link>
-            <Nav.Link href="contact" className={styles.navLink}>Contact</Nav.Link>
+            <NavItem path="/" name="About" />
+            <NavItem path="/skills" name="Skills" />
+            <NavItem path="/projects" name="Projects" />
+            <NavItem path="/contact" name="Contact" />
             </Nav>
         </Navbar.Collapse>
         </Navbar>
